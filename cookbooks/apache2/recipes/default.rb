2784f66002e1d92cb29b653824b10bdd9d1978a8
Aymeric Scherrer <aymeric.scherrer@pm.me>
2019-04-02 13:23:50 +0100

    added roles to webserver

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -1,3 +1,9 @@
+#
+## Cookbook Name:: apache2
+## Recipe:: default
+##
+## Copyright (c) 2016 The Authors, All Rights Reserved.
+
 apt_update 'Update the apt cache daily' do
   frequency 86_400
   action :periodic
@@ -10,7 +16,18 @@ service 'apache2' do
   action [:enable, :start]
 end
 
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
 end
 
